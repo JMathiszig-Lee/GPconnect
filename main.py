52179e89f8ee9bd8184dee87604dced0ef3493e3
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2022-02-09 10:45:53 +0000

    generating ccda with new endpoint

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import ast
-from fastapi import FastAPI, Request
+import json
+import asyncio
+
+from fastapi import FastAPI, Request, Response, applications
 from fastapi.params import Depends
 from fastapi.security import APIKeyCookie
 from starlette.responses import Response, RedirectResponse
@@ -18,7 +21,7 @@ cookie_sec = APIKeyCookie(name="session")
 # replace "redirect_uri" with callback url,
 # which you registered during the app registration
 # this needs to be your own url or ngrok tunnel
-redirect_uri = "http://4aaf-208-127-198-204.ngrok.io/callback"
+redirect_uri = "http://520c-208-127-199-154.ngrok.io/callback"
 
 # replace with your api key
 client_id = os.environ.get("CLIENT_ID")
@@ -61,12 +64,12 @@ async def summary_care_record(token=Depends(cookie_sec)):
     # need ast.literal eval as otherwise token is a string and we get errors
     nhsd = OAuth2Session(client_id, token=ast.literal_eval(token))
 
-    NHS_no = 9995000180
+    NHS_no = 9000000009
     FHir_identifier = f"https://fhir.nhs.uk/Id/nhs-number|{NHS_no}"
     user_restricted_endpoint = nhsd.get(
         f"{SUMMARY_CARE_URL}/DocumentReference", params={"patient": FHir_identifier}
     ).json()
-
+    print(user_restricted_endpoint)
     scr_address = user_restricted_endpoint["entry"][0]["resource"]["content"][0][
         "attachment"
     ]["url"]
@@ -75,3 +78,12 @@ async def summary_care_record(token=Depends(cookie_sec)):
     parsed = await parse_scr.parse_scr(scr)
 
     return {"scr": parsed}
+
+@app.get("/ccda")
+async def return_ccda():
+    #returns a ccda from saved json for now to prevent auth headaches
+    with open("scr.json") as scr_json:
+        scr = json.load(scr_json)
+        xml = await parse_scr.create_ccda(scr)
+        #print(xml)
+        return Response(content=xml, media_type="application/xml")
--- parse_scr.py
+++ parse_scr.py
@@ -1,11 +1,12 @@
 import json
 import asyncio
 from bs4 import BeautifulSoup
-from pprint import PrettyPrinter, pprint
 import xml.etree.cElementTree as ET
 from fhir.resources.bundle import Bundle
+from fhir2ccda import convert_resource
 
 SUMMARY_CARE_URL = "https://sandbox.api.service.nhs.uk/summary-care-record/FHIR/R4"
+
 
 async def parse_scr(scr):
     """
@@ -48,35 +49,63 @@ async def parse_scr(scr):
 async def create_ccda(scr):
     entry = scr["entry"][0]
     references = scr["entry"][1:]
-    
-    def find_reference(reference:str) -> dict:
-        for i in references:
-           
-            if i["fullUrl"] == SUMMARY_CARE_URL + "/" + reference:
-                
-                return i
 
+    def find_reference(reference: str) -> dict:
+        found = [
+            ref
+            for ref in references
+            if ref["fullUrl"] == SUMMARY_CARE_URL + "/" + reference
+        ]
+        return found[0]
 
     ccda = ET.Element("ClinicalDocument")
     ET.SubElement(ccda, "realmcode", code="GB")
     ET.SubElement(
-                ccda,
-                "code",
-                code=entry["resource"]["type"]["coding"][0]["code"],
-                codeSystem="2.16.840.1.113883.6.1",
-                codeSystemName=entry["resource"]["type"]["coding"][0]["system"],
-                displayName=entry["resource"]["type"]["coding"][0]["display"],
+                ccda, "templateId", root="2.16.840.1.11883.10.20.22.1.2", extension="2015-08-01"
             )
+
+    #snomed code
+    ET.SubElement(
+        ccda,
+        "code",
+        code=entry["resource"]["type"]["coding"][0]["code"],
+        codeSystem="2.16.840.1.113883.6.1",
+        codeSystemName=entry["resource"]["type"]["coding"][0]["system"],
+        displayName=entry["resource"]["type"]["coding"][0]["display"],
+    )
+
+    #loinc code
+    ET.SubElement(
+        ccda,
+        "code",
+        code="34133-9",
+        codeSystem="2.16.840.1.113883.6.1",
+        codeSystemName="LOINC",
+    )
+
+    #author section
     author = ET.SubElement(ccda, "author")
     practioner_role = find_reference(entry["resource"]["author"][0]["reference"])
-    practioner = find_reference(practioner_role["resource"]["practitioner"]["reference"])
-    ET.SubElement(author, "assignedAuthor").text = practioner["resource"]["name"][0]["text"]
-    
-    
+    practioner = find_reference(
+        practioner_role["resource"]["practitioner"]["reference"]
+    )
+    ET.SubElement(author, "assignedAuthor").text = practioner["resource"]["name"][0][
+        "text"
+    ]
+
+    #documentation of section
+    doc_of = ET.SubElement(ccda, "documentationOf")
+    service_event = ET.SubElement(doc_of, "serviceEvent", classCode="PCPR")
+    time_covered = ET.SubElement(service_event, "effectiveTime")
+
+    #TODO vlue should be date of birth
+    ET.SubElement(time_covered, "low", value="19750501")
+    #TODO vlue should be date of scr genration
+    ET.SubElement(time_covered, "high", value="20120915")
 
     templateids = {
         "Allergies and Adverse Reactions": {
-            "TemplateID": "2.16.840.1.113883.10.20.22.2.6",
+            "TemplateID": "2.16.840.1.113883.10.20.22.2.6.1",
             "Code": "48765-2",
         },
         "Current Repeat Medications": {
@@ -94,9 +123,11 @@ async def create_ccda(scr):
     }
 
     async def generate_component(section):
-        try:
+        if section["title"] in templateids:
             component = ET.SubElement(ccda, "component")
             sec = ET.SubElement(component, "section")
+
+            #coding system
             ET.SubElement(
                 sec,
                 "code",
@@ -105,13 +136,19 @@ async def create_ccda(scr):
                 codeSystemName="LOINC",
                 displayName=section["title"],
             )
+
+            #root and extension
             ET.SubElement(
                 sec, "templateId", root=templateids[section["title"]]["TemplateID"]
             )
+            ET.SubElement(
+                sec, "templateId", root=templateids[section["title"]]["TemplateID"], extension="2015-08-01"
+            )
+
+            #must contain title
             ET.SubElement(sec, "title").text = section["title"]
             section_html = section["text"]["div"]
-            # soup = BeautifulSoup(section_html, "html.parser")
-            # table = soup.find("table")
+            #must contain text
             text = ET.SubElement(sec, "text")
             text.append(ET.XML(section_html))
 
@@ -119,19 +156,8 @@ async def create_ccda(scr):
                 for entry in section["entry"]:
                     codeable_entry = find_reference(entry["reference"])
                     ent = ET.SubElement(sec, "entry")
-                    ET.SubElement(
-                        ent,
-                        "code",
-                        code=codeable_entry["resource"]["code"]["coding"][0]["code"],
-                        codeSystemName=codeable_entry["resource"]["code"]["coding"][0]["system"],
-                        displayName=codeable_entry["resource"]["code"]["coding"][0]["display"],
-                    )
-
-
-        except:
-            print(section["title"])
-
-    
+                    ent.append(await (convert_resource(codeable_entry)))
+                    
 
     await asyncio.gather(
         *[generate_component(section) for section in entry["resource"]["section"]]
@@ -139,7 +165,9 @@ async def create_ccda(scr):
 
     tree = ET.ElementTree(ccda)
     ET.indent(tree, space="\t", level=0)
-    tree.write("ccda.xml")
+    # ET.dump(tree)
+    xml = ET.tostring(tree.getroot())
+    return xml
 
 
 if __name__ == "__main__":
@@ -147,8 +175,5 @@ if __name__ == "__main__":
         scr = json.load(scr_json)
         asyncio.run(create_ccda(scr))
 
-        bundle = Bundle.parse_obj(scr_json)
-        print(bundle)
-
-    
-
+        # bundle = Bundle.parse_obj(scr_json)
+        # print(bundle)
