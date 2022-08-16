077fd26397566a2b3170dc08db1aca849c321523
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2022-08-16 13:44:59 +0100

    cleaner processing of iti38 query

--- parsesoap.py
+++ parsesoap.py
@@ -6,29 +6,30 @@ import xmltodict
 with open("xml/iti38query.xml") as iti38:
     dom = ElementTree.parse(iti38)
     root = dom.getroot()
+    xmldict = xmltodict.parse(
+        ElementTree.tostring(root),
+        process_namespaces=True,
+        namespaces={
+            "http://www.w3.org/2003/05/soap-envelope": None,
+            "http://www.w3.org/2005/08/addressing": None,
+            "urn:oasis:names:tc:ebxml-regrep:xsd:query:3.0": None,
+            "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0": None,
+        },
+    )
+    # pprint.pprint(xmldict)
+    query = xmldict["Envelope"]["Body"]["AdhocQueryRequest"]["AdhocQuery"]
+    pprint.pprint(query)
 
-    print("adhocquery ID: " + root[1][0][1].get("id"))
-
-    for slot in root[1][0][1]:
-        print(slot.get("name"))
-        for value in slot[0]:
-            print(value.text)
-    # for child in root[1]:
-    #     print(child.tag, child.attrib)
-    #     for i in child:
-    #         print(i.tag, i.attrib)
-    # print(root[1][0].text)
-
-print("xcpd")
-with open("xml/xcpd.xml") as xcpd:
-    dom = ElementTree.parse(xcpd)
-    root = dom.getroot()
-    for child in root[0]:
-        print(child.tag, child.attrib)
-    for child in root[1][0]:
-        print(child.tag, child.attrib)
-    ElementTree.register_namespace("", "urn:hl7-org:v3")
-    xmldict = xmltodict.parse(ElementTree.tostring(root[1][0]))
-    pprint.pprint(xmldict["PRPA_IN201305UV02"]["controlActProcess"]["queryByParameter"])
-    # for child in root[1][0][8].findall(".//"):
-    #     print(child.tag, child.attrib)
+# print("xcpd")
+# with open("xml/xcpd.xml") as xcpd:
+#     dom = ElementTree.parse(xcpd)
+#     root = dom.getroot()
+#     for child in root[0]:
+#         print(child.tag, child.attrib)
+#     for child in root[1][0]:
+#         print(child.tag, child.attrib)
+#     ElementTree.register_namespace("", "urn:hl7-org:v3")
+#     xmldict = xmltodict.parse(ElementTree.tostring(root[1][0]))
+#     pprint.pprint(xmldict["PRPA_IN201305UV02"]["controlActProcess"]["queryByParameter"])
+#     # for child in root[1][0][8].findall(".//"):
+#     #     print(child.tag, child.attrib)
