c6de71a1625beabc282b6b09ceb18405a7d8ac80
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2022-09-14 19:56:40 +0100

    testing

--- SOAP/soap.py
+++ SOAP/soap.py
@@ -1,8 +1,20 @@
+import json
 from urllib.request import Request
 
+import xmltodict
 from fastapi import APIRouter, HTTPException, Request, Response
 
 router = APIRouter(prefix="/SOAP")
+
+NAMESPACES = (
+    {
+        "http://www.w3.org/2003/05/soap-envelope": None,
+        "http://www.w3.org/2005/08/addressing": None,
+        "urn:oasis:names:tc:ebxml-regrep:xsd:query:3.0": None,
+        "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0": None,
+        "urn:ihe:iti:xds-b:2007": None,
+    },
+)
 
 
 @router.post("/iti41")
@@ -23,7 +35,18 @@ async def iti38(request: Request):
     # Response(content=data, media_type="application/xml")
     if content_type == "application/xml":
         body = await request.body()
-        return Response(content=body, media_type="application/xml")
+        dict = xmltodict.parse(
+            body,
+            process_namespaces=True,
+            namespaces={
+                "http://www.w3.org/2003/05/soap-envelope": None,
+                "http://www.w3.org/2005/08/addressing": None,
+                "urn:oasis:names:tc:ebxml-regrep:xsd:query:3.0": None,
+                "urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0": None,
+                "urn:ihe:iti:xds-b:2007": None,
+            },
+        )
+        return dict
     else:
         raise HTTPException(
             status_code=400, detail=f"Content type {content_type} not supported"
